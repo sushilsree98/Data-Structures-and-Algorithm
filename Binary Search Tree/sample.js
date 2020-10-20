51707862ac495697f9255ecb5ebd5e7edfc4f2c5
sushilsree98 <sushil.sree98@gmail.com>
2020-10-20 19:31:52 +0530

    BST insert and find method added

--- Binary Search Tree/sample.js
+++ Binary Search Tree/sample.js
@@ -1,6 +1,6 @@
 class Node {
-  constructor(){
-    this.value = null;
+  constructor(value){
+    this.value = value;
     this.left = null;
     this.right = null
   }
@@ -10,14 +10,64 @@ class BinarySearchTree {
   constructor(){
     this.root = null;
   }
+
+  insert(value){
+    let newNode = new Node(value)
+    if (this.root == null) {
+      this.root = newNode;
+      return this;
+    } else {
+      let currentNode = this.root
+      while(true){
+        if(value === currentNode.value) {
+          return undefined;
+        }
+        if(newNode.value < currentNode.value){
+          if(currentNode.left == null) {
+            currentNode.left = newNode
+            return this
+          }
+          currentNode = currentNode.left
+        } else {
+            if(currentNode.right == null) {
+              currentNode.right = newNode;
+              return this
+            }
+            currentNode = currentNode.right;
+        }
+      }
+    }
+  }
+
+  find(value) {
+    if(this.root == null) return "Not Found";
+    else {
+      let currentNode = this.root
+      let found = false;
+      while(currentNode && !found){
+        if (value < currentNode.value) {
+          currentNode = currentNode.left;
+          }
+       else if (value > currentNode.value) {
+          currentNode = currentNode.right;
+        }else{
+          return true;
+        }
+      }
+      return found;
+    }
+  }
 }
-
+    //     10
+    //   5     15
+    // 2   7 12   17
 let tree = new BinarySearchTree();
-tree.root = new Node()
-tree.root.value = 5;
-tree.root.left = 4;
-tree.root.right = 8
-tree.root.right.left = 6;
-tree.root.right.right = 10
+tree.insert(10)
+tree.insert(15)
+tree.insert(5)
+tree.insert(2)
+tree.insert(7)
+tree.insert(12)
+tree.insert(17)
 
-console.log(tree.root);
+console.log(tree.find(8));
