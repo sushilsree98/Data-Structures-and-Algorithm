308d15ccd8dbbab1751884aecafe08646783ea0f
sushilsree98 <sushil.sree98@gmail.com>
2022-11-03 23:00:19 +0530

    BFS added

--- Graph/graph.js
+++ Graph/graph.js
@@ -64,6 +64,27 @@ class Graph{
         }
         return result
     }
+
+    BFSIterative(start){
+        const queue = [start];
+        const result = [];
+        const visited = {};
+        let currentVertex;
+        visited[start] = true;
+
+        while(queue.length){
+            currentVertex = queue.shift();
+            result.push(currentVertex);
+
+            this.adjacencyList[currentVertex].forEach(neighbor => {
+                if(!visited[neighbor]){
+                    visited[neighbor] = true;
+                    queue.push(neighbor);
+                }
+            });
+        }
+        return result;
+    }
 }
 
 let G = new Graph()
